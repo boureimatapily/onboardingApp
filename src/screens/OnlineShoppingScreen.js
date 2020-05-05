615efa869e9cb451db96ddf19e2269e91c11417a
boureimatapily <boureimatapily1@gmail.com>
2020-05-05 10:18:12 +0000

    MOD add Navigate to AddToCart and Paymaent

--- src/screens/OnlineShoppingScreen.js
+++ src/screens/OnlineShoppingScreen.js
@@ -1,21 +1,21 @@
 import React, { Component } from 'react'
 import { Text, View, StyleSheet, Image, TouchableOpacity } from 'react-native'
-import {Entypo} from '@expo/vector-icons'
+import { Entypo } from '@expo/vector-icons'
 
 export class OnlineShoppingScreen extends Component {
     render() {
-        const {navigation} =this.props
+        const { navigation } = this.props
         return (
             <View style={styles.container}>
                 <View style={styles.titleContainer}>
-                    <Text  style={styles.title}> ONLINE SHOPPING </Text>
+                    <Text style={styles.title}> ONLINE SHOPPING </Text>
                 </View>
 
                 <View style={styles.content}>
                     <Text style={styles.contentText}>
                         Velit ex est velit aute reprehenderit proident irure duis sunt minim sit et.
-                        Reprehenderit cupidatat id proident fugiat aliquip. Enim aute officia officia 
-                        ex.Non est sunt pariatur excepteur dolore in esse proident. Non consectetur 
+                        Reprehenderit cupidatat id proident fugiat aliquip. Enim aute officia officia
+                        ex.Non est sunt pariatur excepteur dolore in esse proident. Non consectetur
                         ad nulla ut incididunt non anim. Adipisicing nulla reprehenderit excepteur ullamco voluptate.
                      </Text>
                 </View>
@@ -24,16 +24,19 @@ export class OnlineShoppingScreen extends Component {
                     <Image source={require('../../assets/shopping.png')} style={styles.image} />
                 </View>
 
-                <TouchableOpacity onPress={()=>
-                {navigation.navigate('AddToCart')}} style={styles.NextButtonContainer}>
+                <TouchableOpacity onPress={() => { navigation.navigate('AddToCart') }} style={styles.NextButtonContainer}>
                     <Text style={styles.NextButton}>Next</Text>
                 </TouchableOpacity>
 
                 <View style={styles.footer}>
                     <Entypo name="progress-one" size={40} style={styles.ProgressIcon} />
-                    <Text style={styles.Skip}>Skip</Text>
+                    <TouchableOpacity  style={styles.Skip}
+                     onPress={() => { navigation.navigate('PaymentSuccessful') }} >
+                    <Text>Skip</Text>
+                    </TouchableOpacity>
+                  
                 </View>
-                
+
             </View>
 
         )
@@ -44,59 +47,59 @@ const styles = StyleSheet.create({
     container: {
         marginHorizontal: 50,
     },
-    titleContainer:{
-        marginVertical:10
+    titleContainer: {
+        marginVertical: 10
     },
-    title:{
-        fontSize:20,
-        fontWeight:"bold",
-        color:"black",
-        
-    },
-    content:{
-        marginBottom:10
-    },
-    contentText:{
-        textAlign:"justify"
-    },
-    ImageContainer:{},
-    image:{
-        width:250,
-        height:250
-    },
-    NextButtonContainer:{
-        backgroundColor:"#b978eb",
-        width:150,
-        height:50,
-        justifyContent:"center",
-        alignItems:"center",
-        borderRadius:30,
-        alignSelf:"center",
-        marginTop:30
+    title: {
+        fontSize: 20,
+        fontWeight: "bold",
+        color: "black",
 
     },
-    NextButton:{
-        fontSize:20,
-        color:"white",
-        fontWeight:"bold"
+    content: {
+        marginBottom: 10
     },
-    footer:{
-        flexDirection:"row",
-        alignItems:"center",
-        marginTop:50
-        
+    contentText: {
+        textAlign: "justify"
     },
-    ProgressIcon:{
-        marginLeft:100,
-        color:"#b978eb",
-        
+    ImageContainer: {},
+    image: {
+        width: 250,
+        height: 250
     },
-    Skip:{
-        position:"absolute",
-        right:0,
-        color:"#c6becc",
-        
-        
+    NextButtonContainer: {
+        backgroundColor: "#b978eb",
+        width: 150,
+        height: 50,
+        justifyContent: "center",
+        alignItems: "center",
+        borderRadius: 30,
+        alignSelf: "center",
+        marginTop: 30
+
+    },
+    NextButton: {
+        fontSize: 20,
+        color: "white",
+        fontWeight: "bold"
+    },
+    footer: {
+        flexDirection: "row",
+        alignItems: "center",
+        marginTop: 50
+
+    },
+    ProgressIcon: {
+        marginLeft: 100,
+        color: "#b978eb",
+
+    },
+    Skip: {
+        position: "absolute",
+        right: 0,
+        color: "#c6becc",
+
+
     },
 })
 
